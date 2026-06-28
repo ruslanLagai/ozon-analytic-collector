9115ea108c24b99330731d5b5aee00317a8ef103
Лагай Руслан Евгеньевич (4059413) <7790Ruslan>
2026-06-28 17:52:18 +0300

    added metrics

---
+++ src/main/resources/migration/V1__add_positions.sql
@@ -0,0 +1,44 @@
+insert into position_entity (id, sku, artikul, archive, name, version)
+values (1, '3389954573', '0000063', false, 'Зонт черный', 0);
+
+insert into position_entity (id, sku, artikul, archive, name, version)
+values (2, '3340294438', '0000062', false, 'Зонт салатовый', 0);
+
+insert into position_entity (id, sku, artikul, archive, name, version)
+values (3, '3340285564', '0000061', false, 'Зонт синий', 0);
+
+insert into position_entity (id, sku, artikul, archive, name, version)
+values (4, '3250075155', '0000058', false, 'Зонт розовый автомат', 0);
+
+insert into position_entity (id, sku, artikul, archive, name, version)
+values (5, '3250068021', '0000057', false, 'Зонт персиковый автомат', 0);
+
+insert into position_entity (id, sku, artikul, archive, name, version)
+values (6, '3250069990', '0000056', false, 'Зонт лавандовый автомат', 0);
+
+insert into position_entity (id, sku, artikul, archive, name, version)
+values (7, '3250052475', '0000055', false, 'Зонт голубой автомат', 0);
+
+insert into position_entity (id, sku, artikul, archive, name, version)
+values (8, '2551437754', '0000051', false, 'Зонт голубой градиент, 6 спиц', 0);
+
+insert into position_entity (id, sku, artikul, archive, name, version)
+values (9, '1589765459', '0000030', false, 'Сумка пляжная молочная', 0);
+
+insert into position_entity (id, sku, artikul, archive, name, version)
+values (10, '1589800338', '0000031', false, 'Сумка пляжная бежевая', 0);
+
+insert into position_entity (id, sku, artikul, archive, name, version)
+values (11, '1589762463', '0000032', false, 'Сумка пляжная розовая', 0);
+
+insert into position_entity (id, sku, artikul, archive, name, version)
+values (12, '1134733705', '0000012', false, 'Зонт серый', 0);
+
+insert into position_entity (id, sku, artikul, archive, name, version)
+values (13, '1134731178', '0000011', false, 'Зонт розовый', 0);
+
+insert into position_entity (id, sku, artikul, archive, name, version)
+values (14, '1134715033', '0000010', false, 'Зонт бежевый', 0);
+
+insert into position_entity (id, sku, artikul, archive, name, version)
+values (15, '1134671293', '0000009', false, 'Зонт лавандовый', 0);
